19fa0775bacf69839e8cdde4986ed88b2dcc9823
Manish-rawat-first <128154980+Manish-rawat-first@users.noreply.github.com>
2023-10-24 17:12:23 +0000

    Num

---
+++ Pract/Replace.js
@@ -0,0 +1,10 @@
+let mr = "Manish Rawat Gaao Jaaye Ga";
+console.log(mr.replaceAll('a','l'))
+let o = [89,90,43,21];
+console.log(o[2])
+let m = 843904.930
+console.log(m.toFixed(1));
+let num = 789.7;
+console.log(num.toPrecision(2))
+let numbe = 1000000
+console.log(numbe.toLocaleString("en-in"))
