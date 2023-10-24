2c5167cf0daaecc2c7695624092edf5e5fd53abe
Manish-rawat-first <128154980+Manish-rawat-first@users.noreply.github.com>
2023-10-24 17:54:16 +0000

    da

--- Pract/Date.js
+++ Pract/Date.js
@@ -1,2 +1,8 @@
 let da = new Date(2002,3,20,5,3);//Let date = new Date(year,month,day(date),Hour,min);
 console.log(da.toLocaleString())
+da = new Date("4-20-2002 09:03:PM");
+console.log(da.toLocaleString())
+console.log(da.toLocaleTimeString())
+
+//How to Find Second in date;
+console.log((Math.floor(da.getTime()/1000)+" Seconds"))
