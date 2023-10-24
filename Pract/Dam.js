20fa820acd41e1f4c49ac2033131e5f559dfa93f
Manish-rawat-first <128154980+Manish-rawat-first@users.noreply.github.com>
2023-10-24 16:38:59 +0000

    Data

--- Pract/Dam.js
+++ Pract/Dam.js
@@ -1,4 +1,11 @@
 let s = (()=>(()=>"Hello"))
 console.log(s().apply())
+console.log(`${s().apply()}`)
 let val = Symbol("He")
 console.log(val)
+s = 457453n
+console.log(typeof s)
+let co = new Number(78)
+console.log(`${co} ${typeof co}`)
+
+
