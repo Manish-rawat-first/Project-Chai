89501452b0ed46dcc47b257dc0e3d0aef050070c
Manish-rawat-first <128154980+Manish-rawat-first@users.noreply.github.com>
2023-10-24 18:00:19 +0000

    Array

---
+++ Pract/Ar.js
@@ -0,0 +1,13 @@
+let array = [12,34,56,78,90];
+console.log(array)
+array.push(87);
+console.log(array);
+array.pop()
+console.log(array);
+array.unshift(1);
+console.log(array);
+array.shift();
+console.log(array);
+console.log(array.slice(0,3));
+console.log(array.splice(0,3));
+console.log(array);
